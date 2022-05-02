3ef8d88257a9679f56cf77bb661b5a860aaec370
SteveOwago <33925746+SteveOwago@users.noreply.github.com>
2022-05-02 17:44:17 +0300

    Database migration files --properties amd products table

---
+++ database/migrations/2022_05_02_143521_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('sku_no');
+            $table->foreignId('category_id');
+            $table->foreignId('store_id');
+            $table->foreignId('property_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
